5c115744f9a06ed87a9334ba4a6697d5cecda475
Agustin Kassis <webmaster@masize.com>
2024-05-18 00:35:48 -0300

    added post examples

--- src/services/prismaTest.ts
+++ src/services/prismaTest.ts
@@ -3,11 +3,13 @@ import { PrismaClient } from '@prisma/client';
 
 // Use custom type
 import { User } from '../types/user';
+import { Post } from '../types/post';
 
 // Instantiate Prisma Client
 const prisma = new PrismaClient();
 
 export class PrismaTest {
+  // Create User
   static async createUser(email: string): Promise<User> {
     console.info('Creating user...');
     const user = await prisma.user.create({
@@ -20,12 +22,14 @@ export class PrismaTest {
     return user;
   }
 
+  // Get all users
   static async listUsers(): Promise<User[]> {
     console.info('Listing users...');
     const users = await prisma.user.findMany();
     return users;
   }
 
+  // Find user by email
   static async findUserByEmail(email: string): Promise<User | null> {
     console.info('Finding user...');
     const user = await prisma.user.findUnique({
@@ -34,5 +38,55 @@ export class PrismaTest {
       },
     });
     return user;
+  }
+
+  // Create post for a user
+  static async createPost(title: string, content: string, userId: number): Promise<Post> {
+    console.info('Creating post...');
+    const post = await prisma.post.create({
+      data: {
+        title: title,
+        content: content,
+        author: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+
+    return post;
+  }
+
+  // Update post by Id
+  static async updatePost(id: number, title: string, content: string): Promise<Post | null> {
+    console.info('Updating post...');
+    const post = await prisma.post.update({
+      where: {
+        id: id,
+      },
+      data: {
+        title: title,
+        content: content,
+      },
+    });
+
+    return post;
+  }
+
+  // Get all posts by a user
+  static async getUserPosts(userId: number): Promise<Post[]> {
+    console.info('Getting user posts...');
+    const posts = await prisma.post.findMany({
+      where: {
+        authorId: userId,
+      },
+      orderBy: {
+        title: 'asc',
+      },
+      take: 10,
+    });
+
+    return posts;
   }
 }
