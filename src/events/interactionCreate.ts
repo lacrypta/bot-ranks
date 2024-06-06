4c9a18e0a6fe961eabd08dc174c81561f895e29f
Rapax <gasparpelayo44@gmail.com>
2024-06-06 17:10:44 -0300

    Update with draft idea

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,53 +1,43 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonComponent, EmbedBuilder, Interaction } from 'discord.js';
+import { Interaction } from 'discord.js';
 import { BotEvent } from '../types/botEvents';
 import { ExtendedClient } from '../types/discordClient';
 
 const event: BotEvent = {
   name: 'interactionCreate',
+  once: false,
   execute: async (interaction: Interaction) => {
     const client = interaction.client as ExtendedClient;
-    if (interaction.isChatInputCommand()) {
-      const command = client.commands.get(interaction.commandName);
-      //   const cooldown = interaction.client.cooldowns.get(
-      //     `${interaction.commandName}-${interaction.guildId}-${interaction.user.username}`,
-      //   );
-      if (!command) return;
-      //   if (command.cooldown && cooldown) {
-      //     if (Date.now() < cooldown) {
-      //       const wait = formatWait(Math.abs(Date.now() - cooldown) / 1000);
 
-      //       interaction.reply(`Tiene que esperar ${wait} para volver a usar este comando.`);
-      //       setTimeout(() => interaction.deleteReply(), 5000);
-      //       return;
-      //     }
-      //     interaction.client.cooldowns.set(
-      //       `${interaction.commandName}-${interaction.user.username}`,
-      //       Date.now() + command.cooldown * 1000,
-      //     );
-      //     setTimeout(() => {
-      //       interaction.client.cooldowns.delete(`${interaction.commandName}-${interaction.user.username}`);
-      //     }, command.cooldown * 1000);
-      //   } else if (command.cooldown && !cooldown) {
-      //     interaction.client.cooldowns.set(
-      //       `${interaction.commandName}-${interaction.guildId}-${interaction.user.username}`,
-      //       Date.now() + command.cooldown * 1000,
-      //     );
-      //   }
-      command.execute(interaction);
-    } else if (interaction.isAutocomplete()) {
-      const command = client.commands.get(interaction.commandName);
-      if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
+    /// Slash Command ///
+    if (interaction.isChatInputCommand()) {
+      const command = client.commands.get(interaction.commandName); // Get command from collection
+      if (!command) return; // If command doesn't exist, return
+      command.execute(interaction); // If command exist, execute it
+    }
+    /// Button ///
+    else if (interaction.isButton()) {
+      console.log(interaction);
+    }
+    /// String Select Menu ///
+    else if (interaction.isStringSelectMenu()) {
+      /// /role-rection ///
+      if (interaction.customId === 'role-reaction') {
+        const selectedRoleId = interaction.values[0];
+        const selectedRole = interaction.guild?.roles.cache.get(selectedRoleId!);
+
+        if (selectedRole) {
+          // TODO: enviar a la base de datos el id del rol con respecto al mensaje
+
+          await interaction.update({
+            content: `You selected the role: ${selectedRole.name}.`,
+            components: [],
+          });
+        }
       }
-    } else if (interaction.isButton()) {
-      console.log(interaction);
-      // TODO
-      // switch (interaction.customId) { }
-    } else if (interaction.isStringSelectMenu()) {
-      console.log(interaction);
-      // TODO
-      // switch (interaction.customId) { }
+      /// Don't implemented String Select Menu ///
+      else {
+        console.log('Interaction is not implemented.');
+      }
     }
   },
 };
