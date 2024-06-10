a96c32addf82fe0021408ab1b063fec02e0a8756
Rapax <gasparpelayo44@gmail.com>
2024-06-10 15:00:38 -0300

    Update command

--- src/commands/roleReaction/roleReaction.ts
+++ src/commands/roleReaction/roleReaction.ts
@@ -9,6 +9,7 @@ import {
   User,
 } from 'discord.js';
 import { Command } from './../../types/command';
+import { prisma } from '../../services/prismaClient';
 
 let message: Message | undefined = undefined;
 
@@ -25,8 +26,6 @@ const roleReaction: Command = {
   execute: async (interaction: CommandInteraction) => {
     const messageId = interaction.options.get('message_id', true).value as string;
     message = await interaction.channel?.messages.fetch(messageId);
-
-    // TODO: enviar a la base de datos el id del mensaje
 
     /// Get role list ///
     const rolesList = interaction.guild?.roles.cache
@@ -55,26 +54,46 @@ const roleReaction: Command = {
       ephemeral: true,
     });
 
+    const prismaGuild = await prisma.guild.create({
+      data: {
+        discordGuildId: interaction.guildId!,
+      },
+    });
+    const prismaChannel = await prisma.channel.create({
+      data: {
+        discordChannelId: interaction.channelId!,
+        guildId: prismaGuild.id,
+      },
+    });
+    const prismaMessage = await prisma.message.create({
+      data: {
+        discordMessageId: messageId,
+        discordInteractionId: interaction.id,
+        channelId: prismaChannel.id,
+      },
+    });
+
     // TODO: call function to wait to execute indeed reaction message with db data
   },
 };
 
-export async function reactionMessage() {
+export async function makeRoleReaction() {
   if (message) {
     try {
       const reactionManager = message.reactions;
       // console.log('Awaiting reaction:', reactionManager.message.reactions.cache);
 
-      eventEmitter.on('messageReactionAdd', async (reaction: MessageReaction, user: User) => {
-        if (message && reaction.message.id === message.id) {
-          try {
-            await message.react(reaction.emoji);
-            console.log(`[roleReactions.ts] Reacted to the message with ${reaction.emoji.name}`);
-          } catch (error) {
-            console.error('[roleReactions.ts] Failed to react to message:', error);
-          }
-        }
-      });
+      // eventEmitter.on('messageReactionAdd', async (reaction: MessageReaction, user: User) => {
+      //   if (message && reaction.message.id === message.id) {
+      //     try {
+      //       await message.react(reaction.emoji);
+      //       console.log(`[roleReactions.ts] Reacted to the message with ${reaction.emoji.name}`);
+      //     } catch (error) {
+      //       console.error('[roleReactions.ts] Failed to react to message:', error);
+      //     }
+      //   }
+      // });
+
       // - [x] wait user reaction
       // - [x] get reaction
       // - [x] react to message with same react
