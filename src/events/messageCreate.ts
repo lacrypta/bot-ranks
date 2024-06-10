c227c0402b962142cdc066fd2f21747227098b8a
Rapax <gasparpelayo44@gmail.com>
2024-06-10 15:01:02 -0300

    Event for tests

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,12 @@
+import { Message } from 'discord.js';
+import { BotEvent } from '../types/botEvents';
+
+const event: BotEvent = {
+  //   name: 'messageCreate',
+  //   once: false,
+  //   execute: async (message: Message) => {
+  //     console.log(`[messageCreate.ts] ${message.author.tag} said: ${message}`);
+  //   },
+};
+
+export default event;
