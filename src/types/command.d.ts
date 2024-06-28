14fd9338b12c8d6bda886b64d7b99f79fb52dba2
Rapax <gasparpelayo44@gmail.com>
2024-06-28 19:02:55 -0300

    Minor fixes

--- src/types/command.d.ts
+++ src/types/command.d.ts
@@ -1,6 +1,7 @@
-import { SlashCommandBuilder, SlashCommandOptionsOnlyBuilder, CommandInteraction } from 'discord.js';
+import { CommandInteraction } from 'discord.js';
+import { SlashCommandBuilder } from '@discordjs/builders';
 
 export interface Command {
-  data: SlashCommandBuilder /* | SlashCommandOptionsOnlyBuilder */;
+  data: SlashCommandBuilder;
   execute: (interaction: CommandInteraction) => Promise<void>;
 }
