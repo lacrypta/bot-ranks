f6be29f0e0564ee2abc629c5c778a6aee2310582
Rapax <gasparpelayo44@gmail.com>
2024-07-03 16:13:55 -0300

    Update ready event for load all guilds the bot is in

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,11 +1,24 @@
 import { BotEvent } from '../types/botEvents';
 import { deployCommands } from '../deployCommands';
+import { cacheService } from '../services/cache';
+import { Client, Guild } from 'discord.js';
 
 const event: BotEvent = {
   name: 'ready',
   once: true,
-  execute: async () => {
+  execute: async (_client: Client) => {
     await deployCommands();
+
+    // Iterate over each guild the bot is in
+    _client.guilds.cache.forEach(async (guild: Guild) => {
+      try {
+        const guildId = guild.id;
+        await cacheService.createGuild(guildId);
+        console.log(`Guild created/updated in the database: ${guild.name} (${guildId})`);
+      } catch (error) {
+        console.error(`Failed to create/update guild: ${guild.name} (${guild.id})`, error);
+      }
+    });
 
     console.log('\x1b[37m\x1b[42m%s\x1b[0m', 'Discord bot ready!');
   },
