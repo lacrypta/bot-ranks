8e102bc5ec768db79a512677242a8d29357c3116
Rapax <gasparpelayo44@gmail.com>
2024-07-10 14:39:16 -0300

    Clean

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,15 @@
-import { Client, GatewayIntentBits, Events, Interaction, Collection, GuildMember } from 'discord.js';
+import { Client, GatewayIntentBits, Collection } from 'discord.js';
 import { ExtendedClient } from './types/discordClient';
 import { readdirSync } from 'fs';
 import { join } from 'path';
 
 import { BotEvent } from './types/botEvents';
-import { PrismaTest } from './services/prismaTest';
 import { commandsList } from './deployCommands';
 import { Command } from './types/command';
-// import readyEvent from './events/ready';
-// import newMessage from './events/newMessage';
 
 require('dotenv').config();
 
 console.info('Hello World');
-
-// (async () => {
-//   // Generate random email
-//   const testEmail = `${Math.random()}@sdfsdfd.com`;
-
-//   // Test create user
-//   await PrismaTest.createUser(testEmail);
-
-//   // Test read users
-//   const users = await PrismaTest.listUsers();
-//   console.info('List Users:', users);
-
-//   // Test find user by email
-//   const user = await PrismaTest.findUserByEmail(testEmail);
-//   console.info('User:', user);
-
-//   // Create Post
-//   const post = await PrismaTest.createPost('Hello World', 'This is a test post', user!.id);
-//   console.info('Post:', post);
-
-//   // Update Post
-//   await PrismaTest.updatePost(post.id, 'Hello World Updated', 'This is an updated test post');
-
-//   // Get posts
-//   const userPosts = await PrismaTest.getUserPosts(user!.id);
-//   console.info('User posts:', userPosts);
-// })();
 
 const client = new Client({
   intents: [
