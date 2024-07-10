fd77ab7fd09f65ae5f450b4bc3359e7df80da361
Rapax <gasparpelayo44@gmail.com>
2024-07-10 14:44:44 -0300

    Add more levels and use cache

--- src/services/temporalLevel.ts
+++ src/services/temporalLevel.ts
@@ -1,7 +1,6 @@
-import { Member } from '@prisma/client';
-import { Message, MessageReaction, User } from 'discord.js';
-import { prisma } from './prismaClient';
-import { time } from 'console';
+import { Member as PrismaMember } from '@prisma/client';
+import { Guild, GuildMember, Message, MessageReaction, User } from 'discord.js';
+import { cacheService } from './cache';
 
 const COLDOWN_MS = 5 * 1000;
 
@@ -13,7 +12,7 @@ interface Level {
 const levels: Level = {
   '1': 500,
   '2': 1000,
-  '3': 2000,
+  '3': 3000,
   '4': 4000,
   '5': 5000,
   '6': 6000,
@@ -21,6 +20,18 @@ const levels: Level = {
   '8': 8000,
   '9': 9000,
   '10': 10000,
+  '11': 11000,
+  '12': 12000,
+  '13': 13000,
+  '14': 14000,
+  '15': 15000,
+  '16': 16000,
+  '17': 17000,
+  '18': 18000,
+  '19': 19000,
+  '20': 20000,
+  '21': 21000,
+  '22': 100000,
 };
 
 enum XpTypes {
@@ -37,11 +48,11 @@ export interface LevelUpStatus {
 }
 
 /// aca
-function canLevelUp(member: Member, xpToAdd: number) {
+function canLevelUp(member: PrismaMember, xpToAdd: number) {
   const nextLevelXp: number = levels[(member.discordTemporalLevel + 1).toString()]!;
 
-  // canLevelUp === true, return the remaining xp after level up.
-  // canLevelUp === false, return the xp to add.
+  // canLevelUp === true, return the remaining xp after level up. (you must asign LevelUpStatus.xpRemaining to member.discordTemporalLevelXp)
+  // canLevelUp === false, return the xp to add. (you must add LevelUpStatus.xpRemaining to member.discordTemporalLevelXp)
   const newXp: number = member.discordTemporalLevelXp + xpToAdd;
   return {
     canLevelUp: newXp >= nextLevelXp,
@@ -50,65 +61,59 @@ function canLevelUp(member: Member, xpToAdd: number) {
   } as LevelUpStatus;
 }
 
-function amountXpToAddMessage(message: Message, member: Member) {
-  const messageLength: number = message.content.length;
-  const newMessageTimestamp: number = message.createdTimestamp;
-  const lastMessageTimestamp: string = member.discordTemporalLevelCooldown;
+function amountXpToAddMessage(_message: Message, _prismaMember: PrismaMember) {
+  const messageLength: number = _message.content.length;
+  const newTimestamp: number = _message.createdTimestamp;
+  const lastTimestamp: string = _prismaMember.discordTemporalLevelCooldown;
 
   const xpType: number = XpTypes.MESSAGE;
-  const deltaTime: number = newMessageTimestamp - parseInt(lastMessageTimestamp);
+  const deltaTime: number = newTimestamp - parseInt(lastTimestamp);
   const lengthMultiplier: number = (Math.random() * messageLength) % 1; // TODO
 
   const amountXpToAdd = Math.floor(xpType * Math.min(1, deltaTime / COLDOWN_MS) + xpType * lengthMultiplier);
 
-  const levelUpStatus: LevelUpStatus = canLevelUp(member, amountXpToAdd);
+  const levelUpStatus: LevelUpStatus = canLevelUp(_prismaMember, amountXpToAdd);
 
   return levelUpStatus;
 }
 
-async function amountXpToAddReaction(reactionTimestamp: number, reactionAuthor: User, messageAuthor: User) {
+async function amountXpToAddReaction(
+  _reactionTimestamp: number,
+  _reactionAuthor: PrismaMember,
+  _messageAuthor: PrismaMember,
+) {
   try {
-    if (reactionAuthor === messageAuthor) {
-      throw new Error('Reaction author is the same as message author');
-    }
-    if (reactionAuthor.bot) {
-      throw new Error('Reaction author is a bot');
+    if (_reactionAuthor === _messageAuthor) {
+      return;
     }
 
     /// Message Author
-    const prismaMessageAuthor = await prisma.member.update({
-      where: {
-        discordMemeberId: messageAuthor.id,
-      },
-      data: {
-        discordTemporalLevelXp: {
-          increment: XpTypes.REACTION_RECEIVE,
-        },
-      },
-    });
+    const prismaMessageAuthor: PrismaMember | null = await cacheService.incrementMemberXp(
+      _messageAuthor,
+      XpTypes.REACTION_RECEIVE,
+      undefined,
+    );
+
+    if (!prismaMessageAuthor) {
+      throw new Error('Message author not found');
+    }
 
     const levelUpStatusMessageAuthor: LevelUpStatus = canLevelUp(prismaMessageAuthor, XpTypes.REACTION_RECEIVE);
 
     /// Reaction Author
-    const prismaReactionAuthor: Member | null = await prisma.member.findUnique({
-      where: {
-        discordMemeberId: reactionAuthor.id,
-      },
-    });
-
-    if (!prismaReactionAuthor) {
+    if (!_reactionAuthor) {
       throw new Error('Reaction author not found');
     }
 
-    const newReactionTimestamp: number = reactionTimestamp;
-    const lastMessageTimestamp: string = prismaReactionAuthor.discordTemporalLevelCooldown;
+    const newTimestamp: number = _reactionTimestamp;
+    const lastTimestamp: string = _reactionAuthor.discordTemporalLevelCooldown;
 
     const xpType: number = XpTypes.REACTION_SEND;
-    const deltaTime: number = newReactionTimestamp - parseInt(lastMessageTimestamp);
+    const deltaTime: number = newTimestamp - parseInt(lastTimestamp);
 
-    const amountXpToAdd = Math.floor(xpType * Math.min(1, deltaTime / COLDOWN_MS));
+    const amountXpToAdd = Math.floor(xpType * Math.min(1, deltaTime / COLDOWN_MS)); // TODO
 
-    const levelUpStatusReactionAuthor: LevelUpStatus = canLevelUp(prismaReactionAuthor, amountXpToAdd);
+    const levelUpStatusReactionAuthor: LevelUpStatus = canLevelUp(_reactionAuthor, amountXpToAdd);
 
     return { reactionAuthor: levelUpStatusReactionAuthor, messageAuthor: levelUpStatusMessageAuthor };
   } catch (error) {
@@ -117,131 +122,103 @@ async function amountXpToAddReaction(reactionTimestamp: number, reactionAuthor: 
 }
 
 /// Functions
-async function addXpMessage(message: Message) {
+async function addXpMessage(_message: Message) {
   try {
-    const memberId = message.author.id;
+    const prismaMember: PrismaMember | null = await cacheService.getMemberByDiscordId(
+      _message.guild!.id,
+      _message.author.id,
+    );
 
-    const member = await prisma.member.findUnique({
-      where: {
-        discordMemeberId: memberId,
-      },
-    });
-
-    if (!member) {
+    if (!prismaMember) {
       throw new Error('Member not found');
     }
 
-    const levelUpStatus: LevelUpStatus = amountXpToAddMessage(message, member);
+    const levelUpStatus: LevelUpStatus = amountXpToAddMessage(_message, prismaMember);
 
     if (levelUpStatus.canLevelUp) {
-      await prisma.member.update({
-        where: {
-          discordMemeberId: memberId,
-        },
-        data: {
-          discordTemporalLevel: levelUpStatus.level,
-          discordTemporalLevelXp: levelUpStatus.xpRemaining,
-          discordTemporalLevelCooldown: message.createdTimestamp.toString(),
-        },
-      });
+      await cacheService.levelUpMember(
+        prismaMember,
+        levelUpStatus.xpRemaining,
+        levelUpStatus.level,
+        _message.createdTimestamp.toString(),
+      );
     } else {
-      await prisma.member.update({
-        where: {
-          discordMemeberId: memberId,
-        },
-        data: {
-          discordTemporalLevelXp: member.discordTemporalLevelXp + levelUpStatus.xpRemaining,
-          discordTemporalLevelCooldown: message.createdTimestamp.toString(),
-        },
-      });
+      await cacheService.incrementMemberXp(
+        prismaMember,
+        levelUpStatus.xpRemaining,
+        _message.createdTimestamp.toString(),
+      );
     }
 
     return levelUpStatus;
   } catch (error) {
-    console.error(`Failed to add xp to member: ${message.author.displayName}`, error);
+    console.error(`Failed to add xp to member: ${_message.author.displayName}`, error);
   }
 }
 
-async function addXpReaction(reaction: MessageReaction, userReacted: User) {
+async function addXpReaction(_reaction: MessageReaction, _reactionAuthor: GuildMember) {
   try {
-    const messageAuthor = reaction.message.author;
-    const reactionAuthor = userReacted;
-
-    if (!messageAuthor || !reactionAuthor) {
-      throw new Error('Message or reaction author not found');
+    if (_reactionAuthor?.user?.bot) {
+      throw new Error('Bot cannot receive xp');
     }
 
-    const prismaMessageAuthor = await prisma.member.findUnique({
-      where: {
-        discordMemeberId: messageAuthor!.id,
-      },
-    });
-    const prismaReactionAuthor = await prisma.member.findUnique({
-      where: {
-        discordMemeberId: reactionAuthor.id,
-      },
-    });
+    const messageAuthorId: string = _reaction.message.author!.id;
+    const discordGuildId: string = _reaction.message.guild!.id;
+
+    if (!messageAuthorId || !_reactionAuthor) {
+      throw new Error('Message author or reaction author not found');
+    }
+
+    const prismaMessageAuthor: PrismaMember | null = await cacheService.getMemberByDiscordId(
+      discordGuildId,
+      messageAuthorId,
+    );
+
+    const prismaReactionAuthor: PrismaMember | null = await cacheService.getMemberByDiscordId(
+      discordGuildId,
+      _reactionAuthor.id,
+    );
 
     if (!prismaMessageAuthor || !prismaReactionAuthor) {
       throw new Error('Member not found');
     }
 
-    const _cooldown: number = Date.now();
-    const _levelUpStatus = await amountXpToAddReaction(_cooldown, reactionAuthor, messageAuthor!);
+    const cooldown: number = Date.now();
+    const levelUpStatus = await amountXpToAddReaction(cooldown, prismaReactionAuthor, prismaMessageAuthor);
 
-    if (!_levelUpStatus) return;
+    if (!levelUpStatus) return;
 
-    if (_levelUpStatus['reactionAuthor'].canLevelUp) {
-      await prisma.member.update({
-        where: {
-          discordMemeberId: reactionAuthor.id,
-        },
-        data: {
-          discordTemporalLevel: _levelUpStatus['reactionAuthor'].level,
-          discordTemporalLevelXp: _levelUpStatus['reactionAuthor'].xpRemaining,
-          discordTemporalLevelCooldown: _cooldown.toString(),
-        },
-      });
+    if (levelUpStatus['reactionAuthor'].canLevelUp) {
+      await cacheService.levelUpMember(
+        prismaReactionAuthor,
+        levelUpStatus['reactionAuthor'].xpRemaining,
+        levelUpStatus['reactionAuthor'].level,
+        cooldown.toString(),
+      );
     } else {
-      await prisma.member.update({
-        where: {
-          discordMemeberId: reactionAuthor.id,
-        },
-        data: {
-          discordTemporalLevelXp: {
-            increment: _levelUpStatus['reactionAuthor'].xpRemaining,
-          },
-          discordTemporalLevelCooldown: _cooldown.toString(),
-        },
-      });
+      await cacheService.incrementMemberXp(
+        prismaReactionAuthor,
+        levelUpStatus['reactionAuthor'].xpRemaining,
+        cooldown.toString(),
+      );
     }
 
-    if (_levelUpStatus['messageAuthor'].canLevelUp) {
-      await prisma.member.update({
-        where: {
-          discordMemeberId: messageAuthor.id,
-        },
-        data: {
-          discordTemporalLevel: _levelUpStatus['messageAuthor'].level,
-          discordTemporalLevelXp: _levelUpStatus['messageAuthor'].xpRemaining,
-          discordTemporalLevelCooldown: _cooldown.toString(),
-        },
-      });
+    if (levelUpStatus['messageAuthor'].canLevelUp) {
+      await cacheService.levelUpMember(
+        prismaMessageAuthor,
+        levelUpStatus['messageAuthor'].xpRemaining,
+        levelUpStatus['messageAuthor'].level,
+        cooldown.toString(),
+      );
     } else {
-      await prisma.member.update({
-        where: {
-          discordMemeberId: messageAuthor.id,
-        },
-        data: {
-          discordTemporalLevelXp: {
-            increment: _levelUpStatus['messageAuthor'].xpRemaining,
-          },
-          discordTemporalLevelCooldown: _cooldown.toString(),
-        },
-      });
+      await cacheService.incrementMemberXp(
+        prismaMessageAuthor,
+        levelUpStatus['messageAuthor'].xpRemaining,
+        cooldown.toString(),
+      );
     }
 
-    return _levelUpStatus;
+    return levelUpStatus;
   } catch (error) {
     console.error(`Failed to add xp to member`, error);
 
